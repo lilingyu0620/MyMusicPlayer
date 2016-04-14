b97a9b72d887b55d3b3694719ddf71379c5ada6d
lly <lingyuli@sohu-inc.com>
2016-04-14 10:14:28 +0800

    新建工程

---
+++ MusicDemo/MusicDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicDemo
+//
+//  Created by lly on 16/4/14.
+//  Copyright © 2016年 lly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicDemo/MusicDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MusicDemo
+//
+//  Created by lly on 16/4/14.
+//  Copyright © 2016年 lly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
